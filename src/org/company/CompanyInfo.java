071f80f12c77e5a9e651387e09927db83109f7f4
ProjectBalaji <bb2505200@gmail.com>
2022-10-13 11:51:34 +0530

    project

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,24 @@
+package org.company;
+
+public class CompanyInfo {
+	
+  private void companyName() {
+	 System.out.println("Dominos");
+	 
+     }
+  private void companyId() {
+     System.out.println("Jubl");
+     
+     }
+  private void companyAddress() {
+     System.out.println("Pallikaranai");
+     
+     }
+  public static void main(String[] args) {
+	CompanyInfo c = new CompanyInfo();
+	 c.companyName();
+	 c.companyId();
+	 c.companyAddress();
+
+     }
+}
